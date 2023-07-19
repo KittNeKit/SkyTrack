ea41f85bafc806eaae08d795a83951b85938ed82
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-19 21:47:25 +0300

    Implement user model

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'airport',
     'flight',
+    'order',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -102,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
