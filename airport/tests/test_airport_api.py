c0be1d58be969a7f4ef800a0fd7b3445963f3027
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-20 15:08:35 +0300

    implement tests for airport

---
+++ airport/tests/test_airport_api.py
@@ -0,0 +1,116 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from airport.models import Airport
+from airport.serializers import AirportSerializer
+
+AIRPORT_URL = reverse("airport:airport-list")
+
+
+def sample_airport(**params):
+    defaults = {
+        "name": "Test airport",
+        "closest_bit_city": "Test City",
+    }
+    defaults.update(params)
+
+    return Airport.objects.create(**defaults)
+
+
+def detail_url(airport_id):
+    return reverse("airport:airport-detail", args=[airport_id])
+
+
+class UnauthenticatedAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_not_required(self):
+        res = self.client.get(AIRPORT_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+class AuthenticatedAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_airport(self):
+        sample_airport()
+        sample_airport()
+
+        res = self.client.get(AIRPORT_URL)
+
+        airport = Airport.objects.order_by("id")
+        serializer = AirportSerializer(airport, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_retrieve_airport_detail(self):
+        airport = sample_airport()
+
+        url = detail_url(airport.id)
+        res = self.client.get(url)
+
+        serializer = AirportSerializer(airport)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_airport_forbidden(self):
+        payload = {
+            "name": "Test Forbidden",
+            "closest_bit_city": "Test Forbidden",
+        }
+        res = self.client.post(AIRPORT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "admin@admin.com", "testpass", is_staff=True
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_create_airport(self):
+        payload = {
+            "name": "Test airport",
+            "closest_bit_city": "Test City",
+        }
+        res = self.client.post(AIRPORT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        movie = Airport.objects.get(id=res.data["id"])
+        for key in payload.keys():
+            self.assertEqual(payload[key], getattr(movie, key))
+
+    def test_put_airport_not_allowed(self):
+        payload = {
+            "name": "Test airport",
+            "closest_bit_city": "Test City",
+        }
+
+        airport = sample_airport()
+        url = detail_url(airport.id)
+
+        res = self.client.put(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_delete_airport_not_allowed(self):
+        airport = sample_airport()
+        url = detail_url(airport.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
---
+++ airport/tests/test_route_api.py
@@ -0,0 +1,108 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from airport.models import Route
+from airport.serializers import RouteSerializer
+from airport.tests.test_airport_api import sample_airport
+
+ROUTE_URL = reverse("airport:route-list")
+
+
+def sample_route(**params):
+    defaults = {
+        "source": sample_airport(),
+        "destination": sample_airport(),
+        "distance": 300
+    }
+    defaults.update(params)
+
+    return Route.objects.create(**defaults)
+
+
+def detail_url(route_id):
+    return reverse("airport:route-detail", args=[route_id])
+
+
+class UnauthenticatedRouteApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_not_required(self):
+        res = self.client.get(ROUTE_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+class AuthenticatedRouteApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_route(self):
+        sample_route()
+        sample_route()
+
+        res = self.client.get(ROUTE_URL)
+
+        route = Route.objects.order_by("id")
+        serializer = RouteSerializer(route, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_retrieve_route_detail(self):
+        route = sample_route()
+
+        url = detail_url(route.id)
+        res = self.client.get(url)
+
+        serializer = RouteSerializer(route)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_route_forbidden(self):
+        payload = {
+            "source": sample_airport(),
+            "destination": sample_airport(),
+            "distance": 403
+        }
+        res = self.client.post(ROUTE_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminRouteApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "admin@admin.com", "testpass", is_staff=True
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_put_route_not_allowed(self):
+        payload = {
+            "source": sample_airport().id,
+            "destination": sample_airport().id,
+            "distance": 405
+        }
+
+        route = sample_route()
+        url = detail_url(route.id)
+
+        res = self.client.put(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_delete_route_not_allowed(self):
+        route = sample_route()
+        url = detail_url(route.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
