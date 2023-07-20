d5c05e71f82ed777ec3a3fe3d0161402587b4099
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-20 13:23:31 +0300

    Implement view for user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+@extend_schema_view(
+    post=extend_schema(description="Creating user endpoint."),
+)
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+@extend_schema_view(
+    get=extend_schema(
+        description="Detail info about current user endpoint."
+    ),
+    put=extend_schema(description="Updating the current user endpoint."),
+    patch=extend_schema(
+        description="Partially update the current user endpoint."
+    ),
+)
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
