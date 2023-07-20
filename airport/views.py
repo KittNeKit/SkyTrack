bf2156d447c9ab6db246c75c492e6e1e03e3366c
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-20 13:23:23 +0300

    Added permission class

--- airport/views.py
+++ airport/views.py
@@ -1,14 +1,21 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from airport.models import Airport, Route
+from airport.permissions import IsAdminOrIfUserReadOnly
 from airport.serializers import AirportSerializer, RouteSerializer
 
 
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    permission_classes = (IsAdminOrIfUserReadOnly,)
+    authentication_classes = (JWTAuthentication,)
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all().select_related("source", "destination")
     serializer_class = RouteSerializer
+    permission_classes = (IsAdminOrIfUserReadOnly,)
+    authentication_classes = (JWTAuthentication,)
+
