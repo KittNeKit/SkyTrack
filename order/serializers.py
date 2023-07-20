1dd7bcfc1ae6fd3efb8031fdda08031efb878e9d
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-20 14:32:12 +0300

    Added order creation, delete ticket serializer

--- order/serializers.py
+++ order/serializers.py
@@ -1,13 +1,8 @@
 from django.core.exceptions import ValidationError
+from django.db import transaction
 from rest_framework import serializers
 
 from order.models import Order, Ticket
-
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ("id", "created_at", "user")
 
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -23,4 +18,20 @@ class TicketSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ticket
-        fields = ("id", "row", "seat", "flight", "order")
+        fields = ("row", "seat", "flight")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at")
+
+    @transaction.atomic()
+    def create(self, validated_data):
+        tickets_data = validated_data.pop("tickets")
+        order = Order.objects.create(**validated_data)
+        for ticket_data in tickets_data:
+            Ticket.objects.create(order=order, **ticket_data)
+        return order
